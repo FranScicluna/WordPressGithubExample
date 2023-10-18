593ac09d2fa8bed5105d6fd829c43c0cfdc9a0e9
FranScicluna <francescoscicluna@gmail.com>
2023-10-18 09:19:08 +0200

    wp_footer and posts display

--- app/public/wp-content/themes/firstTheme/index.php
+++ app/public/wp-content/themes/firstTheme/index.php
@@ -1,16 +1,28 @@
 <?php get_header(); ?>
 
-<img style="width: 100%; height: 700px; padding:50px 0px 50px 0px" src="https://images.pexels.com/photos/3802510/pexels-photo-3802510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Car Main Image, Lamborghini">
+<!-- The Loop -->
 
-<div class="imgGrid">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-3"></div>
-			<div class="col-3"></div>
-			<div class="col-3"></div>
-			<div class="col-3"></div>
+<h1>Index</h1>
+
+<div class="container">
+	<div class="row">
+		<div class="col-8">
+			<?php if(have_posts()) : while (have_posts()) : the_post() ?> <!-- have_posts() checks if there is posts to show / : is the sameas {} -->
+		
+			<h2>
+				<?php the_title(); ?>
+			</h2>
+
+			<small>
+				<?php the_date(); ?> by <?php the_author_posts_link();?>
+			</small>
+
+			<?php endwhile; ?>
+			<? endif; ?>
+		</div>	
+		<div class="col-4">
+			<!-- Prepped for Sidebar -->
 		</div>
 	</div>
 </div>
-
 <?php get_footer(); ?>
